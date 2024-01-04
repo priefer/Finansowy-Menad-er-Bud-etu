b7ff08ed8830b34589b0edd44a2fe1a0d1ec3bef
Kamil Priefer <pan@priefer.pl>
2024-01-04 21:21:12 +0100

    Dodaj pliki projektów.

---
+++ Finansowy Menadżer Budżetu/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Finansowy_Menadżer_Budżetu.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
